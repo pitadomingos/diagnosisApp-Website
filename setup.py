9ea1bda69ff77e7aee9971761dda55775a506df9
pitadomingos <pita13035@gmail.com>
2024-07-13 11:20:43 +0000

    setup.y added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my-python-project',
+    version='3.10.0',
+    description='diagnosisApp-Website',
+    author='Pita Domingos',
+    author_email='pita.domingos@zd044.onmicrosoft.com',
+    packages=find_packages()
+    )
